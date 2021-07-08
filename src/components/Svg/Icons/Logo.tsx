f32ab88453178831ab0c3eccc99a8253cbfee8d7
Mariam Saidik <w1543592@westminster.ac.uk>
2021-07-08 14:27:33 +0100

    commit

--- src/components/Svg/Icons/Logo.tsx
+++ src/components/Svg/Icons/Logo.tsx
@@ -4,8 +4,8 @@ import { SvgProps } from "../types";
 
 const Icon: React.FC<SvgProps> = (props) => {
   return (
-    <Svg viewBox="0 0 32 32" {...props}>
-      <image width="32" height="32" href="/images/egg/LogoTextNewDark.png"/>
+    <Svg viewBox="0 0 132 32" {...props}>
+      <image width="132" height="32" href="/images/egg/LogoTextNewDark.png"/> {/* used to be 32x32 logo.png */}
     </Svg>
   );
 };
