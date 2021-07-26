2ee30c721223ba352d8a984433fa94772d763948
Mariam Saidik <w1543592@westminster.ac.uk>
2021-07-26 05:12:14 +0100

    commit

--- src/components/Tag/StyledTag.tsx
+++ src/components/Tag/StyledTag.tsx
@@ -7,7 +7,7 @@ interface ThemedProps extends TagProps {
 }
 
 const getThemeTextColor = ({ outline, variant = "primary", theme }: ThemedProps) =>
-  outline ? getColor(variant, theme) : "#12a46e";
+  outline ? getColor(variant, theme) : "#fcdc04";
 
 export const StyledTag = styled.div<ThemedProps>`
   align-items: center;
