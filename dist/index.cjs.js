15ef202da6d9dcdfba557a1df6dbd805c4f3feab
Mariam Saidik <w1543592@westminster.ac.uk>
2021-08-05 03:14:19 +0100

    commit

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2840,7 +2840,7 @@ var brandColors = {
 var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#f7f7f7", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#dbdbdb", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#040c0a", textDisabled: "#BDC2C4", textSubtle: "#040c0a", borderColor: "#E9EAEB", card: "#dbdbdb", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
-var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#c04697", background: "#190000", backgroundDisabled: "#3c3742", contrast: "#dbdbdb", invertedContrast: "#191326", input: "#483f5a", primaryDark: "#0098A1", tertiary: "#353547", text: "#040c0a", textDisabled: "#666171", textSubtle: "#040c0a", borderColor: "#524B63", card: "#c40000", gradients: {
+var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#c04697", background: "#190000", backgroundDisabled: "#3c3742", contrast: "#dbdbdb", invertedContrast: "#191326", input: "#483f5a", primaryDark: "#0098A1", tertiary: "#353547", text: "#cecccc", textDisabled: "#666171", textSubtle: "#cecccc", borderColor: "#524B63", card: "#630000", gradients: {
         bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
     } });
 
